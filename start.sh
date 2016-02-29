3c5e6bb58813ff8a80b0b194518d8eaa2edfbf93
Vincent Galloy <vgalloy@excilys.com>
2016-02-29 16:12:31 +0100

    [ FIX ] : Style

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-
-docker-compose up -d
-
 echo -n "password : "
 read -s password
 
+docker-compose up -d
+
 docker exec computer-database-jenkins docker login --username="vgalloy" --email="vgalloy@excilys.com" --password="$password"
 
+echo "Jenkins tourne (par defaut) sur http://localhost:10080"
